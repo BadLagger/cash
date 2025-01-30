6d39eaaea156454843aa11aeffebc0338c82a961
Vladimir Hrechko <grechko_vo@navis.ru>
2025-01-30 16:49:04 +0300

    Добавил класс для работы с категориями расходов и доходов в класс пользователя. Всё сохраняется и загружается из файла db

--- src/main/java/sf/hrechko/cash/UserDb.java
+++ src/main/java/sf/hrechko/cash/UserDb.java
@@ -11,128 +11,281 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class UserDb extends FileDb {
 
-	public class User {
-		private String login;
-		private String password;
+    public class CashCategory {
+	private String name;
+	private int value;
+	
+	CashCategory() {
+	    name = "";
+	    value = 0;
+	}
+	
+	CashCategory(String name) {
+	    this.name = name;
+	    value = 0;
+	}
+	
+	CashCategory(String name, int value) {
+	    this.name = name;
+	    this.value = value;
+	}
+	
+	String getName() {
+	    return name;
+	}
+	
+	int getValue() {
+	    return value;
+	}
+	
+	void setValue(int val) {
+	    value = val;
+	}
+	
+	String getJsonStr() {
+	    return String.format("\"%s\": %d", name, value);
+	}
+    }
 
-		public User() {
-			login = "";
-			password = "";
-		}
+    public class User {
+	private String login;
+	private String password;
+	private Set<CashCategory> revenue = new HashSet<>(); // доход
+	private Set<CashCategory> spending = new HashSet<>(); // расход
 
-		public User(String name, String pswd) {
-			login = name;
-			password = pswd;
+	private void createDefaultCashCategories() {
+	    revenue.add(new CashCategory("Пополнение"));
+	    revenue.add(new CashCategory("Перевод"));
+	    spending.add(new CashCategory("Снятие"));
+	    spending.add(new CashCategory("Перевод"));
+	}
+	
+	private CashCategory getCategoryFromSet(String name, Set<CashCategory> set) {
+	    CashCategory result = null;
+	    
+	    for (var element : set) {
+		if (element.getName().equals(name)) {
+		    result = element;
+		    break;
 		}
-
-		public String getLogin() {
-			return login;
-		}
-
-		public boolean checkPassword(String pswd) {
-			return password.equals(pswd);
-		}
-
-		public boolean equals(String name) {
-			return login.equals(name);
-		}
-
-		public boolean equals(User user) {
-			return login.equals(user.getLogin());
-		}
-
-		String getJsonStr() {
-			return String.format("\"%s\" : {\"password\":\"%s\"}", login, password);
-		}
+	    }
+	    
+	    return result;
+	}
+	
+	public User() {
+	    login = "";
+	    password = "";
+	    createDefaultCashCategories();
 	}
 
-	private JsonNode rootNode = null;
-	private Set<User> userList = new HashSet<>();
-
-	public UserDb(String filePath) {
-		super(filePath);
+	public User(String name, String pswd) {
+	    login = name;
+	    password = pswd;
+	    createDefaultCashCategories();
 	}
 
-	public boolean init() {
-		if (!load()) {
-			if (!file.exists())
-				try {
-					System.out.format("Создать ");
-					file.createNewFile();
-					setStr("{}");
-					if (!save())
-						return false;
-				} catch (IOException e) {
-					e.printStackTrace();
-					return false;
-				}
-		}
-
-		return loadJson();
+	public String getLogin() {
+	    return login;
 	}
 
-	public void close() {
-		String outData = "{";
-		int userNumber = userList.size();
-		int count = 0;
-
-		for (var user : userList) {
-			outData += user.getJsonStr();
-			if (++count < userNumber) {
-				outData += ",";
-			}
-		}
-		outData += "}";
-		setStr(outData);
-		save();
+	public boolean checkPassword(String pswd) {
+	    return password.equals(pswd);
 	}
 
-	public boolean isUserPresent(String user) {
-		for (var usr : userList) {
-			if (usr.getLogin().equals(user))
-				return true;
-		}
-		return false;
+	public boolean equals(String name) {
+	    return login.equals(name);
 	}
 
-	public boolean isUserPresent(User user) {
-		return isUserPresent(user.getLogin());
+	public boolean equals(User user) {
+	    return login.equals(user.getLogin());
+	}
+	
+	public boolean setIncome(CashCategory cash) {
+	    CashCategory foundCategory = getIncomeByName(cash.getName());
+	    if (foundCategory == null)
+		return revenue.add(cash);
+	    else {
+		int currentValue = foundCategory.getValue() + cash.getValue();
+		foundCategory.setValue(currentValue);
+	    }
+	    return true;
+	}
+	
+	public boolean setOutcome(CashCategory cash) {
+	    CashCategory foundCategory = getOutcomeByName(cash.getName());
+	    if (foundCategory == null)
+		return revenue.add(cash);
+	    else {
+		int currentValue = foundCategory.getValue() + cash.getValue();
+		foundCategory.setValue(currentValue);
+	    }
+	    return true;
+	}
+	
+	public CashCategory getIncomeByName(String name) {
+	    return getCategoryFromSet(name, revenue);
+	}
+	
+	public CashCategory getOutcomeByName(String name) {
+	    return getCategoryFromSet(name, spending);
+	}
+	
+	public String[] getIncomeNames() {
+	    String[] result = new String[revenue.size()];
+	    int count = 0;
+	    
+	    for (var rev : revenue) {
+		result[count++] = rev.getName();
+	    }
+	    return result;
+	}
+	
+	public String[] getOutcomeNames() {
+	    String[] result = new String[spending.size()];
+	    int count = 0;
+	    
+	    for (var sp : spending) {
+		result[count++] = sp.getName();
+	    }
+	    return result;
+	}
+	
+	public int getBalance() {
+	    int balance = 0;
+	    
+	    for (var rev : revenue) {
+		balance += rev.value;
+	    }
+	    
+	    for (var sp : spending) {
+		balance -= sp.value;
+	    }
+	    return balance;
 	}
 
-	public User getUser(String name) {
-		for (var user : userList) {
-			if (user.getLogin().equals(name))
-				return user;
-		}
-		return null;
+	public String getJsonStr() {
+	    String outStr = String.format("\"%s\" : {\"password\":\"%s\",\"revenue\":{", login, password);
+	    int count = 0;
+	    for (var rev : revenue) {
+		outStr += String.format("%s", rev.getJsonStr());
+		if (++count < revenue.size())
+		    outStr += ",";
+	    }
+	    outStr += "},\"spending\":{";
+	    count = 0;
+	    for (var sp : spending) {
+		outStr += String.format("%s", sp.getJsonStr());
+		if (++count < revenue.size())
+		    outStr += ",";
+	    }
+	    outStr += "}}";
+	    return outStr;
 	}
+	
+    }
 
-	public boolean addUser(User newUser) {
-		if (isUserPresent(newUser))
-			return false;
+    private JsonNode rootNode = null;
+    private Set<User> userList = new HashSet<>();
 
-		return userList.add(newUser);
-	}
+    public UserDb(String filePath) {
+	super(filePath);
+    }
 
-	private boolean loadJson() {
+    public boolean init() {
+	if (!load()) {
+	    if (!file.exists())
 		try {
-			rootNode = new ObjectMapper().readTree(getStr());
-			var keys = rootNode.fieldNames();
-			while (keys.hasNext()) {
-				String userName = keys.next();
-				var userData = rootNode.get(userName);
-				String userPswd = userData.get("password").asText();
-				// Todo other params
-				User user = new User(userName, userPswd);
-				userList.add(user);
-			}
-			return true;
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+		    System.out.format("Создать ");
+		    file.createNewFile();
+		    setStr("{}");
+		    if (!save())
+			return false;
+		} catch (IOException e) {
+		    e.printStackTrace();
+		    return false;
 		}
-		return false;
 	}
+
+	return loadJson();
+    }
+
+    public void close() {
+	String outData = "{";
+	int userNumber = userList.size();
+	int count = 0;
+
+	for (var user : userList) {
+	    outData += user.getJsonStr();
+	    if (++count < userNumber) {
+		outData += ",";
+	    }
+	}
+	outData += "}";
+	setStr(outData);
+	save();
+    }
+
+    public boolean isUserPresent(String user) {
+	for (var usr : userList) {
+	    if (usr.getLogin().equals(user))
+		return true;
+	}
+	return false;
+    }
+
+    public boolean isUserPresent(User user) {
+	return isUserPresent(user.getLogin());
+    }
+
+    public User getUser(String name) {
+	for (var user : userList) {
+	    if (user.getLogin().equals(name))
+		return user;
+	}
+	return null;
+    }
+
+    public boolean addUser(User newUser) {
+	if (isUserPresent(newUser))
+	    return false;
+
+	return userList.add(newUser);
+    }
+
+    private boolean loadJson() {
+	try {
+	    rootNode = new ObjectMapper().readTree(getStr());
+	    var keys = rootNode.fieldNames();
+	    while (keys.hasNext()) {
+		String userName = keys.next();
+		var userData = rootNode.get(userName);
+		String userPswd = userData.get("password").asText();
+		User user = new User(userName, userPswd);
+		// Todo refactor
+		var revenue = userData.get("revenue");
+		var revCategories = revenue.fieldNames();
+		while (revCategories.hasNext()) {
+		    String revName = revCategories.next();
+		    int revValue = revenue.get(revName).asInt();
+		    user.setIncome(new CashCategory(revName, revValue));
+		}
+		var spending = userData.get("spending");
+		var spCategories = spending.fieldNames();
+		while (spCategories.hasNext()) {
+		    String spName = spCategories.next();
+		    int spValue = spending.get(spName).asInt();
+		    user.setOutcome(new CashCategory(spName, spValue));
+		}
+		userList.add(user);
+	    }
+	    return true;
+	} catch (JsonMappingException e) {
+	    e.printStackTrace();
+	} catch (JsonProcessingException e) {
+	    e.printStackTrace();
+	}
+	return false;
+    }
 
 }
